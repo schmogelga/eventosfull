a6149f2ebdf630c4c41fa26da025c2e77360a951
Gabriel Schmogel <gabriel.schmogel@dbserver.com.br>
2024-07-20 11:23:44 -0300

    commit teste pipeline

--- src/main/java/com/schmogel/eventosfull/application/adapter/EventoAdapter.java
+++ src/main/java/com/schmogel/eventosfull/application/adapter/EventoAdapter.java
@@ -4,7 +4,10 @@ import java.util.Optional;
 
 import com.schmogel.eventosfull.application.dto.EventoResponse;
 import com.schmogel.eventosfull.domain.model.Evento;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class EventoAdapter {
 
     public static EventoResponse entityToDto(Evento evento) {
@@ -17,6 +20,5 @@ public class EventoAdapter {
                                 .map(inscricoes -> inscricoes.stream().map(InscricaoAdapter::entityToDTO).toList())
                                 .orElse(null))
                 .build();
-
     }
 }
